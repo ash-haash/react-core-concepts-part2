33e60a6caab94d52d0a58beb9a3fbc324b7f1623
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-03-08 16:05:46 +0000

    useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Counter from './Counter';
 import Team from './Team';
+import Users from './Users';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
   return (
     <>
       <h2>React Core Concepts 2</h2>
+      <Users></Users>
       <Team></Team>
       <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
